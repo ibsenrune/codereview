fc8351f1bf9476110155a2fd99a390ed79762d82
Rune Ibsen <rui@copenhagensoftware.com>
2016-05-08 20:42:46 +0200

    Now retrieves all fields in suggestions

--- codereview.js
+++ codereview.js
@@ -10,12 +10,15 @@ var setAuthenticationHeaders = function(){
             queryTokenizer: Bloodhound.tokenizers.whitespace,
             limit: 8,
             remote: { 
-                url : 'https://codereview.search.windows.net/indexes/articles/docs/suggest?suggesterName=sg&fuzzy=true&api-version=2015-02-28&search=%QUERY',
+                url : 'https://codereview.search.windows.net/indexes/articles/docs/suggest?suggesterName=sg&fuzzy=true&api-version=2015-02-28&$select=*&search=%QUERY',
                 wildcard : '%QUERY',
                 transform: function(response){
                     return $.map(response.value, function(result) {
+                        console.log(result);
                         return {
-                          value: result["@search.text"]
+                          name: result["@search.text"],
+                          summary: result["summary"],
+                          type: result["type"]
                         };
                     });
                 }
@@ -25,13 +28,22 @@ var setAuthenticationHeaders = function(){
         articles.initialize();
 
         $('#remote .typeahead').typeahead(null, {
-            display: 'value',
-            source: articles
+            source: articles,
+            display: function(data){ return data.name; },
+            templates: {
+                empty: '<div>Unable to find anything based on your search criteria.</div>',
+                suggestion: function(data){ 
+                    return '<div>' +
+                                '<div class="suggestion-name">'  + data.name + '</div>' +
+                                '<div class="suggestion-stats">' + data.type + '</div>' +
+                            '</div>';
+                }
+            },
         }).on('typeahead:selected', function(obj, datum){
             console.log(obj);
             console.log(datum);
 
-            $('#results').html('<h2>Result</h2>'+ datum.value);
+            $('#results').html('<h2>' + datum.name + '</h2>' + '<section>' + datum.summary + '</section>');
         });
     },
   focus = function(){
